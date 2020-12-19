3e3f2ba290342b43df061b416121076efe1ede21
Ahmed Eid <a.eid@yandex.com>
2020-12-19 11:27:26 +0200

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/self-closing-comp': 'off',
+  },
+};
---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, ScrollView, View, Text} from 'react-native';
+
+function App() {
+  return <View style={{flex: 1, backgroundColor: 'blue'}}></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
